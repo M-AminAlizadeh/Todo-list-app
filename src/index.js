592bc79625584446d8978d2efddab87378204ae7
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-05-05 23:19:20 +0330

    Build structure of todo list and style it

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import printMe from './print.js';
 import './style.css';
 
 function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+  // const element = document.createElement('div');
+  // const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
-  element.classList.add('hello');
-  btn.onclick = printMe;
+  // btn.innerHTML = 'Click me and check the console!';
+  // element.classList.add('hello');
+  // btn.onclick = printMe;
 
-  element.appendChild(btn);
+  // element.appendChild(btn);
 
-  return element;
+  return 'element';
 }
 
 document.body.appendChild(component());
