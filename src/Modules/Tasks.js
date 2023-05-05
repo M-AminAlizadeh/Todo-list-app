e2d27c105e2014b69a21f4bf87abde709ceacfc5
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-05-05 23:35:48 +0330

    Import data from module and display them on DOM using js

---
+++ src/Modules/Tasks.js
@@ -0,0 +1,21 @@
+const tasks_container = document.querySelector('.tasks-container ');
+const Tasks = [
+  {
+    id: 1,
+    task_name: 'task1',
+  },
+  {
+    id: 2,
+    task_name: 'task2',
+  },
+  {
+    id: 3,
+    task_name: 'task3',
+  },
+  {
+    id: 4,
+    task_name: 'task4',
+  },
+];
+
+export { tasks_container, Tasks };
