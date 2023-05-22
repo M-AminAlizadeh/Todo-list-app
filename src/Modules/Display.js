48ef193c18609e36b8e8ad83077b2dbd229ff956
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-05-22 17:31:31 +0330

    Change checkbox styles

--- src/Modules/Display.js
+++ src/Modules/Display.js
@@ -6,21 +6,13 @@ const Display = (tasks) => {
   let content = '';
   tasks.forEach((task) => {
     content += `
-        <li class="task-container my-2 d-flex justify-content-between align-items-center" id="${
-          task.index
-        }">
+        <li class="task-container my-2 d-flex justify-content-between align-items-center" id="${task.index}">
           <div class="task-left-side-container">
-            <input type="checkbox" name="${task.taskContent}" id="${
-      task.index
-    }" class="checkbox" ${task.completed ? 'checked' : null}/>
-            <input type="text" class="task text mx-2 border-0  ${
-              task.completed ? 'delete' : null
-            }" value="${task.taskContent}" readonly />
+            <input type="checkbox" name="${task.taskContent}" id="${task.index}" class="checkbox form-check-input" ${task.completed ? 'checked' : null}/>
+            <input type="text" class="task text mx-2 border-0  ${task.completed ? 'delete' : null}" value="${task.taskContent}" readonly />
           </div>
           <div class="task-right-side-container">
-          <button class="btn btn-success edit-btn" id="${
-            task.index
-          }">Edit</button>
+          <button class="btn btn-success edit-btn" id="${task.index}">Edit</button>
           <img
             src="https://img.icons8.com/pastel-glyph/25/ff0000/trash.png"
             class="icon trash-icon" id="${task.index}"/>
