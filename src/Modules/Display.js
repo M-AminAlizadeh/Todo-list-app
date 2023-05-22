337a5f72ee032e3baf9e9d878e3972135a8314e2
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-05-22 17:22:16 +0330

    Change Edit button and Trash icon style

--- src/Modules/Display.js
+++ src/Modules/Display.js
@@ -7,20 +7,22 @@ const Display = (tasks) => {
   tasks.forEach((task) => {
     content += `
         <li class="task-container my-2 d-flex justify-content-between align-items-center" id="${
-  task.index
-}">
+          task.index
+        }">
           <div class="task-left-side-container">
             <input type="checkbox" name="${task.taskContent}" id="${
-  task.index
-}" class="checkbox" ${task.completed ? 'checked' : null}/>
+      task.index
+    }" class="checkbox" ${task.completed ? 'checked' : null}/>
             <input type="text" class="task text mx-2 border-0  ${
-  task.completed ? 'delete' : null
-}" value="${task.taskContent}" readonly />
+              task.completed ? 'delete' : null
+            }" value="${task.taskContent}" readonly />
           </div>
           <div class="task-right-side-container">
-          <button class="edit-btn" id="${task.index}">Edit</button>
+          <button class="btn btn-success edit-btn" id="${
+            task.index
+          }">Edit</button>
           <img
-            src="https://img.icons8.com/pastel-glyph/20/null/trash.png"
+            src="https://img.icons8.com/pastel-glyph/25/ff0000/trash.png"
             class="icon trash-icon" id="${task.index}"/>
           </div>
         </li>`;
